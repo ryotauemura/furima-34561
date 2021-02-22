40bcbb9435e2aedb84d8bca14b051cd7e33cef4d
ryotauemura <taurin276@gmail.com>
2021-02-22 16:57:17 +0900

    ユーザーモデルのテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname {Fakker::Name.nickname}
+    nickname {Faker::Name.name}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
+    last_name {Faker::Name.last_name}
+    first_name {Faker::Name.first_name}
+    last_name_kana {'テスト'}
+    first_name_kana {'テスト'}
+    birthday {"2020-01-01"}
   end
 end
