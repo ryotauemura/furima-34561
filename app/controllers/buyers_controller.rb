0e6335e406d7e6e9d925a67f6c66b0d7f15de2e0
ryotauemura <taurin276@gmail.com>
2021-03-04 17:37:53 +0900

    ログイン状態によってのリダイレクトまで

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,16 +1,17 @@
 class BuyersController < ApplicationController
-  before_action :authenticate_user!, only: :index 
+  before_action :authenticate_user!, only: [:index] 
+  before_action :set_item, only:[:index,:create]
+  before_action :redirect_buyer, only:[:index]
+
   def index 
-    @item = Item.find(params[:item_id])
     @buyer_shipping = BuyersShippings.new
   end
 
   def new
-    
+
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buyer_shipping = BuyersShippings.new(buyer_params)
     if @buyer_shipping.valid?
       pay_item
@@ -34,6 +35,16 @@ class BuyersController < ApplicationController
       card: buyer_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_buyer
+      if current_user.id == @item.user.id || @item.buyer.present?
+       redirect_to root_path
+      end
   end
   
 end
